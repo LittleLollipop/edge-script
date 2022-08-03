61d5ec3e6f530f8212d42461c2b956b5b646fd36
Sai <kldtks@live.com>
2022-08-03 19:44:50 +0800

    ubuntu install edge

---
+++ install/ubuntu.sh
@@ -0,0 +1,23 @@
+# These base components need to be installed
+sudo apt update
+
+sudo apt install socat -y
+sudo apt install conntrack -y
+
+sudo apt install ebtables -y
+sudo apt install ipset -y
+sudo apt install ipvsadm -y
+
+
+# Install KubeSphere
+curl -sfL https://get-kk.kubesphere.io | VERSION=v2.2.1 sh -
+chmod +x kk
+
+sudo su
+./kk create cluster --with-kubernetes v1.22.10 --with-kubesphere v3.3.0
+kubectl logs -n kubesphere-system $(kubectl get pod -n kubesphere-system -l 'app in (ks-install, ks-installer)' -o jsonpath='{.items[0].metadata.name}') -f
+
+####Install Desktop
+sudo helm repo add edge-plus https://littlelollipop.github.io/awesome-helm-charts/
+sudo helm repo update 
+sudo helm install my-edge-desktop edge-plus/edge-desktop
